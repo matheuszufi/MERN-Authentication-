96e85fbae602ed1d451e42b953b345324283953b
Matheus Zufi Leite <matheuszufi@gmail.com>
2022-10-31 18:25:47 -0300

    up

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const colors = require('colors')
+const dotenv = require('dotenv').config()
+const {errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+const PORT = process.env.PORT || 5000
+const app = express()
+
+connectDB()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.get('/', (req, res) => {
+    res.status(201).json({ message: 'Welcome to the Support Desk API'})
+})
+
+app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
